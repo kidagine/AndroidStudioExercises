6b89cd13f382f1d0ca8ff99a63d09a4d6f2aaa7c
kidagine <36992613+kidagine@users.noreply.github.com>
2020-03-09 15:05:49 +0100

    Added the ability to add a profile picture through the phone camera app.

--- SwordzList/app/src/main/java/com/example/swordzlist/Activities/DetailsActivity.java
+++ SwordzList/app/src/main/java/com/example/swordzlist/Activities/DetailsActivity.java
@@ -1,22 +1,42 @@
 package com.example.swordzlist.Activities;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 
 import com.example.swordzlist.Entities.Sword;
 import com.example.swordzlist.R;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class DetailsActivity extends AppCompatActivity {
+
+    private static final int CAMERA_REQUEST = 1888;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
 
     EditText dtxName;
     EditText dtxPrice;
     EditText dtxEmail;
+    ImageButton ibtnProfilePicture;
     Button btnSave;
     Button btnEmail;
     Sword swordItem;
@@ -26,6 +46,7 @@ public class DetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstance);
         setContentView(R.layout.activity_detail);
 
+        ibtnProfilePicture = findViewById(R.id.ibtn);
         dtxName = findViewById(R.id.etName);
         dtxPrice = findViewById(R.id.etPhone);
         dtxEmail = findViewById(R.id.etEmail);
@@ -33,6 +54,12 @@ public class DetailsActivity extends AppCompatActivity {
         btnEmail = findViewById(R.id.btnEmail);
         setSwordInformation();
 
+        ibtnProfilePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeProfilePicture();
+            }
+        });
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -52,6 +79,48 @@ public class DetailsActivity extends AppCompatActivity {
 
         dtxName.setText(swordItem.getName());
         dtxPrice.setText(String.valueOf(swordItem.getPrice()));
+    }
+
+    private void changeProfilePicture(){
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
+        }
+        else
+        {
+            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraIntent, CAMERA_REQUEST);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_PERMISSION_CODE)
+        {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+            else
+            {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK)
+        {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            ibtnProfilePicture.setImageBitmap(photo);
+        }
     }
 
     private void save(){
